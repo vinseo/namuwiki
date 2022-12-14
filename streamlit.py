789b2e142fcec62b7ce458f493e49fb6b60598c7
vinseo <120566552+vinseo@users.noreply.github.com>
2022-12-14 20:32:22 +0900

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -8,6 +8,7 @@ import plotly.express as px
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
 from bokeh.plotting import figure
+from datetime import datetime
 
 #data loading
 # st.cache를 이용하여 데이터 로딩을 하는 함수
@@ -17,7 +18,25 @@ def load_data(filename):
     return data
 
 #df0 = load_data("")
+df_culture = load_data('dataculture.csv')
+df_social = load_data('datasocial.csv')
+df_academic = load_data('dataacademic.csv')
 #df = df0.copy()
+
+# date, time 칼럼 합치고 데이터타입 datetime으로 변경
+df_culture['datetime'] = df_culture['date'] + " " + df_culture['time']
+df_culture = df_culture.drop(['date', 'time'], axis=1)
+df_culture['datetime'] = pd.to_datetime(df_culture['datetime'])
+
+df_social['datetime'] = df_social['date'] + " " + df_social['time']
+df_social = df_social.drop(['date', 'time'], axis=1)
+df_social['datetime'] = pd.to_datetime(df_social['datetime'])
+
+df_academic['datetime'] = df_academic['date'] + " " + df_academic['time']
+df_academic = df_academic.drop(['date', 'time'], axis=1)
+df_academic['datetime'] = pd.to_datetime(df_academic['datetime'])
+
+st.write(df_social) #삭제
 
 st.subheader("2022-2 데이터 저널리즘 과제전")
 st.title("나무위키 활용 학습의 가능성과 한계")
